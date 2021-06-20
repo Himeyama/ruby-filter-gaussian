701d0bbc64e8f95177c32aecaa1d1564f03dd122
Murata Mitsuharu <hikari.photon+mygit@gmail.com>
2021-06-21 01:26:38 +0900

    並列計算サンプル

--- test/test.rb
+++ test/test.rb
@@ -8,8 +8,7 @@ files = "scipy.ndimage.gaussian_filter1d.example.%s.bin"
 
 open(files % "x", "rb") do |f|
     x = f.read.unpack("d*")
-    y3 = Gaussian.filter1d(x, 3)
-    y6 = Gaussian.filter1d(x, 6)
+    y3, y6 = Gaussian.filter1d([x, x], [3, 6])
 
     open(files % "y3", "rb") do |fy3|
         sciy3 = fy3.read.unpack("d*")
