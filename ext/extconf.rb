a190a54138bee633c1b6c7dde9efce54505e237d
Murata Mitsuharu <hikari.photon+mygit@gmail.com>
2021-06-21 17:00:22 +0900

    NArray

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -1,2 +1,8 @@
 require 'mkmf'
+
+$CPPFLAGS += " -I#{ENV["HOME"]}/.rbenv/versions/3.0.1/lib/ruby/gems/3.0.0/extensions/x86_64-linux/3.0.0/numo-narray-0.9.1.9/numo"
+
+
+# p "#{ENV["HOME"]}/.rbenv/versions/3.0.1/lib/ruby/gems/3.0.0/extensions/x86_64-linux/3.0.0/numo-narray-0.9.1.9/numo/numo/narray.h"
+
 create_makefile "Gaussian"
