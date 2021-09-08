0acc806fb15ea76c1bd83cae96eda672feb47ae4
Murata Mitsuharu <hikari.photon+dev@gmail.com>
2021-09-08 16:30:41 +0900

    narray path

--- ext/Gaussian/extconf.rb
+++ ext/Gaussian/extconf.rb
@@ -1,7 +1,7 @@
 require 'mkmf'
 
 narray_dir = Gem.find_files("numo").map{|e| e.include?("narray") ? e : false}.select{|e| e}.to_a[0]
-dir_config('narray', narray_dir, narray_dir)
-have_library("narray")
+$INCFLAGS += " -I#{narray_dir}"
+$libs += " #{narray_dir}/narray.so"
 
 create_makefile "Gaussian"
